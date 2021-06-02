db48358d88760bb6381d690e0a0dcb7fe24c7e10
Patrick Lin <tzuyao0112@gmail.com>
2021-06-03 00:37:18 +0800

    add filtering

--- read.py
+++ read.py
@@ -6,8 +6,16 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print('檔案讀取完了，總共有', len(data) '筆資料')
+print('檔案讀取完了，總共有', len(data), '筆資料')
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
-	print('留言的平均長度為', sum_len/len(data))
+print('留言的平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
